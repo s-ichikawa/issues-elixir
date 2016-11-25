70c6e3c9e227d138add05541efac51c07a69bc7f
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-11-25 22:04:32 +0900

    13.6

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
 """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -36,5 +38,14 @@ defmodule Issues.CLI do
     end
   end
 
-  
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+"""
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 end
