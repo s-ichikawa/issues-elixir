392dccf2ec8807a4480190bbb9542fb1c52f0ab2
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-12-01 20:30:59 +0900

    13.11

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,7 +9,7 @@ defmodule Issues.CLI do
   the various functions that end up generating a
   table of the last _n_ issues in a github project
 """
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
